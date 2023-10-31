66de34a8673730be89dcef2f65f5dfbbc1dffb11
Caio Alves <caioalvesfig@gmail.com>
2023-10-31 20:21:44 -0300

    final changes

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -1,6 +1,6 @@
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const studentsService = require('../services/students.service');
+const Joi = require('joi')
+const bcrypt = require('bcrypt')
+const studentsService = require('../services/students.service')
 
 const registerSchema = Joi.object({
   name: Joi.string().min(6).max(100).required(),
@@ -8,47 +8,47 @@ const registerSchema = Joi.object({
   email: Joi.string().max(100).email().required(),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
   repeatPassword: Joi.string().valid(Joi.ref('password')).required(),
-  role: Joi.string().required()
-});
+  isStudent: Joi.number().valid(0, 1).required()
+})
 
 exports.register = async (req, res, next) => {
-  const { error, value } = registerSchema.validate(req.body);
+  const { error, value } = registerSchema.validate(req.body)
   if (error) {
     return res.status(400).send({
       success: 0,
       error: error.details[0].message
-    });
+    })
   }
 
   try {
-    const hashedPassword = await bcrypt.hash(value.password, 10); // Hash the password with a salt factor of 10
+    const hashedPassword = await bcrypt.hash(value.password, 10)
 
     const data = {
       name: value.name,
       username: value.username,
       email: value.email,
-      password: hashedPassword, // Store the hashed password in the database
-      role: value.role
-    };
+      password: hashedPassword,
+      isStudent: value.isStudent
+    }
 
     studentsService.register(data, (err, results) => {
       if (err) {
-        console.error(err);
+        console.error(err)
         return res.status(500).send({
           success: 0,
           error: 'Internal Server Error'
-        });
+        })
       }
       return res.status(200).send({
         success: 1,
         data: results
-      });
-    });
+      })
+    })
   } catch (error) {
     console.error(error);
     return res.status(500).send({
       success: 0,
       error: 'Internal Server Error'
-    });
+    })
   }
-};
+}
--- routes/students.route.js
+++ routes/students.route.js
@@ -5,6 +5,66 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/register', studentsController.register)
+/**
+ * @swagger
+ * /students/register:
+ *   post:
+ *      description: Used to register student
+ *      tags:
+ *          - students
+ *      parameters:
+ *          - in: body
+ *            name: Student
+ *            description: Student data
+ *            schema:
+ *              type: object
+ *              required:
+ *                 - name
+ *                 - username
+ *                 - email
+ *                 - password
+ *                 - repeatPassword
+ *                 - isStudent
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                      minLength: 6
+ *                      maxLength: 100
+ *                      example: John Doe
+ *                      description: Student's full name.
+ *                  username:
+ *                      type: string
+ *                      minLength: 6
+ *                      maxLength: 30
+ *                      example: johndoe
+ *                      unique: true
+ *                      description: Username to be used in the login.
+ *                  email:
+ *                      type: string
+ *                      maxLength: 100
+ *                      example: john@doe.com
+ *                      unique: true
+ *                      description: Valid email address.
+ *                  password:
+ *                      type: string
+ *                      pattern: "^[a-zA-Z0-9]{3,30}$"
+ *                      example: abc123
+ *                      description: Alphanumeric password between 3 and 30 characters.
+ *                  repeatPassword:
+ *                      type: string
+ *                      example: abc123
+ *                      description: Re-enter the password to confirm. Must match the 'password' field.
+ *                  isStudent:
+ *                      type: integer
+ *                      enum: [0, 1]
+ *                      description: Indicates whether the user is a student or not. 0 - Not a student, but a teacher (false). 1 - Student (true).
+ *      responses:
+ *          '200':
+ *              description: Student added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 
 module.exports = router
- 
