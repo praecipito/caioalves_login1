2ea20cac07da19f7fdc26f4a00cd28c15074a822
Caio Alves <caioalvesfig@gmail.com>
2023-10-31 01:29:42 -0300

    folders and files

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ import express from 'express'
 
 const app = express()
 
-app.use('/test', (req, res) => {
-  console.log('Hello from the middleware')
-  res.status(200).send('Success')
-})
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
